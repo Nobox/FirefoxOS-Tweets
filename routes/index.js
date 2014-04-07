8e2da1e8ab492888092fe2da5d9c3ea953e654eb
Javier Velazquez <javieravelazquezhernandez@gmail.com>
2014-04-07 15:05:14 -0400

    Implemented interval to buffer the amount the time in which tweets are rendered
    Also implemented was the database to store and retrieve past tweets with which to render the initial page load

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,16 @@
 app = require('../app');
 
 app.get('/', function(req, res) {
-  res.render('index', { title: 'FirefoxOS Tweets' });
+
+    env = require('habitat').load('.env');
+    Datastore = require('nedb');
+    var db = new Datastore({ filename: 'db/' + env.get('search_query') });
+    db.loadDatabase(function(err) {
+
+        db.find({}).sort({ created_at: -1 }).limit(5).exec(function(err, tweets) {
+            res.render('index', { title: 'FirefoxOS Tweets', tweets : tweets });
+        });
+    });
 });
 
 require('./user');
